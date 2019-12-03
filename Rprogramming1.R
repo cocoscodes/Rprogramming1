d16f9b7d425bfc1f822592253b9255d0844eb6f0
cocoscodes <solis_acevedo@hotmail.com>
2019-12-03 17:15:41 +0100

    latest

--- Rprogramming1.R
+++ Rprogramming1.R
@@ -1,1 +1,28 @@
 getwd()
+
+myfunction <- function(){
+  x <- rnorm(100)
+  mean(x)
+}
+
+ls()# shows the elements in your environment
+
+myfunction()
+
+dir()# shows the files in your directory
+
+?source("Rprogramming1.R")
+
+second <- function(x){
+  x + rnorm(length(x))
+}
+
+second(10)
+
+?dim()# dimensions of an object
+
+
+# notes on Week 1 -----
+# if you would like to add a number with a text type use a capital L next to it "1L"
+# "Inf" is a numerical value representing infinity
+# NaN - undefined value
