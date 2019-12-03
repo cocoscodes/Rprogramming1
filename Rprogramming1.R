bc90bb72189715c855022f9d3b23efa18609e8f8
cocoscodes <solis_acevedo@hotmail.com>
2019-12-03 17:39:04 +0100

    latest

--- Rprogramming1.R
+++ Rprogramming1.R
@@ -26,3 +26,25 @@ second(10)
 # if you would like to add a number with a text type use a capital L next to it "1L"
 # "Inf" is a numerical value representing infinity
 # NaN - undefined value
+# Atributtes - names, dimnames, dimensions, class, lenght, other metadata
+attributes()
+
+# to create vectors one uses the c() function
+x <- c(1+0i, 2+4i)
+
+y <- vector("numeric", length = 10)
+y
+
+# Explicit Coercion
+# Objects can be explicitly coerced from one class to another using the as.* functions, if available.
+x <- 0:6
+class(x)
+
+as.numeric(x)
+
+as.logical(x)
+
+as.character(x)
+
+x <- list(1,"a",TRUE,1+4i)
+x
