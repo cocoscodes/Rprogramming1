e45fc5ec79fddf06474c5ba08bb6cd410f953cf6
a.solis <solis.acevedo@gmail.com>
2019-12-07 09:48:34 +0100

    latest 191297

--- swirl1.R
+++ swirl1.R
@@ -19,3 +19,41 @@ bye()
 
 # hitting up arrow recycles commands!!!
 # typing first 2 letters and tab key will provide a list of variables (auto-completion)
+
+getwd()
+setwd
+ls()
+
+dir() or list.files() # look into your files in your working directory
+
+args() # sees the arguments of a function
+
+dir.create("testdir") # creates a directory in the current WD
+
+file.create("mytest.R") # creates a file in your dir
+
+file.exists("mytest.R") # checks if a file exists
+
+file.info("mytest.R") # access file info
+
+file.info("mytest.R")$mode # to grab specific items.
+
+file.rename("mytest.R","mytest2.R") # change the file name
+
+file.copy("mytest2.R","mytest3.R") # copy file
+
+file.path("mytest3.R") # Provide the relative path to the file
+
+file.path('folder1','folder2') # construct file and directory paths that are independent of the operating system your R code
+
+dir.create(file.path("testdir2","testdir3"),recursive = TRUE) # create directories and subdirectories by changing recursive argument to TRUE
+
+setwd(old.dir) # go back to an original working directory SAVED AS A VARIABLE
+
+file.remove("testdir/")
+
+
+
+
+
+
